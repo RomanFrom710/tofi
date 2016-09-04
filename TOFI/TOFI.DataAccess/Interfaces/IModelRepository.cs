2b883c3c89d40d3afcb27dfc16fd0565cd9b319f
Pavel Kindruk <kinpa200296@yandex.ru>
2016-09-04 14:35:30 +0300

    Added necessary definitions to DAL.

---
+++ TOFI/TOFI.DataAccess/Interfaces/IModelRepository.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections.Generic;
+
+namespace DAL.Interfaces
+{
+    public interface IModelRepository<TModel> : IRepository
+        where TModel : IModel
+    {
+        TModel GetModel(int id);
+
+        IEnumerable<TModel> GetAllModels();
+    }
+}
