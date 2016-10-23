a22a6127363d69dcc72b78206983484ff94aa0b0
Alex <alex.limontov@gmail.net>
2016-10-23 17:14:36 +0300

    added base query and command repository along with MyRepository as example [should be deleted soon]

---
+++ TOFI/TOFI.DataAccess/Repositories/BaseRepository/BaseQueryRepository.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DAL.Repositories.BaseRepository
+{
+    public class BaseQueryRepository: IBaseQueryRepository
+    {
+        public void Dispose()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
