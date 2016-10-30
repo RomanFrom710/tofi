181cd0d1534552df4dd4a2b4dc4e04ce353b3052
Roman Chaley <romanchaley@gmail.com>
2016-10-30 19:27:22 +0300

    Undo unwanted change, remove some commented code

--- TOFI/TOFI.Web/App_Start/IdentityConfig.cs
+++ TOFI/TOFI.Web/App_Start/IdentityConfig.cs
@@ -97,11 +97,6 @@ namespace TOFI.Web
         {
         }
 
-        /*public override Task<ClaimsIdentity> CreateUserIdentityAsync(AuthUser user)
-        {
-            return user.GenerateUserIdentityAsync((ApplicationUserManager)UserManager);
-        }*/
-
         public static ApplicationSignInManager Create(IdentityFactoryOptions<ApplicationSignInManager> options, IOwinContext context)
         {
             return new ApplicationSignInManager(context.GetUserManager<ApplicationUserManager>(), context.Authentication);
--- TOFI/TOFI.Web/App_Start/Startup.Auth.cs
+++ TOFI/TOFI.Web/App_Start/Startup.Auth.cs
@@ -14,8 +14,7 @@ namespace TOFI.Web
         // For more information on configuring authentication, please visit http://go.microsoft.com/fwlink/?LinkId=301864
         public void ConfigureAuth(IAppBuilder app)
         {
-            // Configure the db context, user manager and signin manager to use a single instance per request
-            //app.CreatePerOwinContext(ApplicationDbContext.Create);
+            // Configure user manager and signin manager to use a single instance per request
             app.CreatePerOwinContext<ApplicationUserManager>(ApplicationUserManager.Create);
             app.CreatePerOwinContext<ApplicationSignInManager>(ApplicationSignInManager.Create);
 
