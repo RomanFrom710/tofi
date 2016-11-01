7f035e4f7641b8264d318d1c1f454a8175241e86
Alex <alex.limontov@gmail.net>
2016-11-01 22:33:43 +0300

    user repositories

---
+++ TOFI/TOFI.DataAccess/Repositories/UserRepository/IUserQueryRepository.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Threading.Tasks;
+using DAL.Models;
+
+namespace DAL.Repositories.UserRepository
+{
+    public interface IUserQueryRepository: IBaseQueryRepository
+    {
+        IUser GetUser(int id);
+
+        IQueryable<IUser> GetUsers();
+    }
+}
---
+++ TOFI/TOFI.DataAccess/Repositories/UserRepository/UserCommandRepository.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using DAL.Contexts;
+using DAL.Models;
+using DAL.Repositories.BaseRepository;
+
+namespace DAL.Repositories.UserRepository
+{
+    class UserCommandRepository: BaseCommandRepository, IUserCommandRepository
+    {
+        private readonly TofiContext _context;
+        private readonly DbSet<IUser> _db;
+
+        public UserCommandRepository(TofiContext context): base(context)
+        {
+            _context = context;
+            _db = context.Set<IUser>();
+        }
+
+        public IUser AddUser(IUser user)
+        {
+            _db.Add(user);
+            return user;
+        }
+    }
+}
