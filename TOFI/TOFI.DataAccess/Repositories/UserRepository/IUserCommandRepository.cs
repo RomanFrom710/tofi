c08631c4515df292b25086cabc63fe1180c2b1b0
Alex <alex.limontov@gmail.com>
2016-11-01 22:33:43 +0300

    user repositories

--- TOFI/TOFI.DataAccess/Repositories/UserRepository/IUserCommandRepository.cs
+++ TOFI/TOFI.DataAccess/Repositories/UserRepository/IUserCommandRepository.cs
@@ -3,12 +3,13 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using DAL.Models;
 using DAL.Repositories.BaseRepository;
 
-namespace DAL.Repositories.MyRepository
+namespace DAL.Repositories.UserRepository
 {
-    public class MyQueryRepository: BaseQueryRepository, IMyQueryRepository
+    public interface IUserCommandRepository: IBaseCommandRepository
     {
-       
+        IUser AddUser(IUser user);
     }
 }
---
+++ TOFI/TOFI.DataAccess/Repositories/UserRepository/UserQueryRepository.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Runtime.Remoting.Contexts;
+using System.Text;
+using System.Threading.Tasks;
+using DAL.Contexts;
+using DAL.Models;
+using DAL.Repositories.BaseRepository;
+
+namespace DAL.Repositories.UserRepository
+{
+    public class UserQueryRepository: BaseQueryRepository, IUserQueryRepository
+    {
+        private readonly TofiContext _context;
+        private readonly DbSet<IUser> _db;
+
+        public UserQueryRepository(TofiContext context)
+        {
+            _context = context;
+            _db = context.Set<IUser>();
+        }
+
+
+        public IUser GetUser(int id)
+        {
+            return _db == null ? null : _db.Find(id);
+        }
+
+        public IQueryable<IUser> GetUsers()
+        {
+            return _db;
+        }
+    }
+}
