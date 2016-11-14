6ddb4ec9efaf62969446cf1622a77e582b6d3f46
Maksim Shlandakou <shlandakov@fksis.by>
2016-11-14 11:41:23 +0300

    Mapping skeleton added

---
+++ TOFI/TOFI.BusinessLayer/Mapping/MapperConfig.cs
@@ -0,0 +1,14 @@
+﻿using AutoMapper.Configuration;
+
+namespace BLL.Mapping
+{
+    public static class MapperConfig
+    {
+        public static void LoadConfig(ref MapperConfigurationExpression config)
+        {
+            DAL.Mapping.MapperConfig.LoadConfig(ref config);
+
+            //cfg.CreateMap<Source, Dest>(); - simple mapping example
+        }
+    }
+}
