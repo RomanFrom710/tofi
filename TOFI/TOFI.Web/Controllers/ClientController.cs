cdd956a3b4784a9b0e7b8dce2cdb95a1607fa209
Roman Chaley <romanchaley@gmail.com>
2016-12-18 17:34:43 +0300

    Added the client page

---
+++ TOFI/TOFI.Web/Controllers/ClientController.cs
@@ -0,0 +1,25 @@
+﻿using System.Web.Mvc;
+using BLL.Services.Client.ViewModels;
+
+namespace TOFI.Web.Controllers
+{
+    public class ClientController : Controller
+    {
+        [HttpGet]
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Index(ClientViewModel client)
+        {
+            return View(client);
+        }
+
+        public ActionResult Credit()
+        {
+            return View();
+        }
+    }
+}
