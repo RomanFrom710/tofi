908a5253d11483164dfb4a2cddcc1465906629cc
Roman Chaley <romanchaley@gmail.com>
2016-12-18 17:34:43 +0300

    Added the client page

--- TOFI/TOFI.BusinessLayer/Services/Client/ViewModels/ClientViewModel.cs
+++ TOFI/TOFI.BusinessLayer/Services/Client/ViewModels/ClientViewModel.cs
@@ -1,8 +1,17 @@
-﻿using BLL.Services.User.ViewModels;
+﻿using System.ComponentModel.DataAnnotations;
+using BLL.Services.User.ViewModels;
 
 namespace BLL.Services.Client.ViewModels
 {
     public class ClientViewModel : UserViewModel
     {
+        [Required]
+        public string DocumentNumber { get; set; }
+
+        [Required]
+        public string DocumentInfo { get; set; }
+
+        [Required]
+        public string Address { get; set; }
     }
 }
