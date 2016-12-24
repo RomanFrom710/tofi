dd6dd83af37c0617322db71bccfc3af14039153a
Alex <alex.limontov@gmail.com>
2016-12-24 23:01:07 +0300

    no cascade update error; ClientModel updated

--- TOFI/TOFI.DataAccess/Models/Client/ClientModel.cs
+++ TOFI/TOFI.DataAccess/Models/Client/ClientModel.cs
@@ -1,15 +1,38 @@
 ﻿using System.ComponentModel.DataAnnotations.Schema;
 using DAL.Models.User;
+using System;
 
 namespace DAL.Models.Client
 {
     [Table("Clients")]
     public class ClientModel : Model
     {
+        public string Address { get; set; }
+
+        public string TelephoneNumber { get; set; }
+
+        public string PassportNumber { get; set; }
+
+        public string PassportId { get; set; }
+
+        public string Authority { get; set; }
+
+        public DateTime IssueDate { get; set; }
+
+        public DateTime ExpirationDate { get; set; }
+
+        public Sex Sex { get; set; }
+
         #region Virtual Properties
 
         public virtual UserModel User { get; set; }
 
         #endregion
+    }
+
+    public enum Sex
+    {
+        Female,
+        Male
     }
 }
