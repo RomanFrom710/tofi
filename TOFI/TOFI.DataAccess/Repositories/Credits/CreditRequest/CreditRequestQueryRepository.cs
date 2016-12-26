5e64f7d6f809068faabd164eec1bfd0b97df281b
Alex <alex.limontov@gmail.com>
2016-12-27 00:41:06 +0300

    added CreditRequestModel; renamed UserAction -> UserActionModel

---
+++ TOFI/TOFI.DataAccess/Repositories/Credits/CreditRequest/CreditRequestQueryRepository.cs
@@ -0,0 +1,15 @@
+﻿using DAL.Contexts;
+using DAL.Models.Credits.CreditRequest;
+using DAL.Repositories.Credits.CreditRequest;
+using DAL.Repositories.Model;
+using TOFI.TransferObjects.Credits.CreditRequest.DataObjects;
+
+namespace DAL.Repositories.Credits.CreditAccount
+{
+    public class CreditRequestQueryRepository : ModelQueryRepository<CreditRequestModel, CreditRequestDto>, ICreditRequestQueryRepository
+    {
+        public CreditRequestQueryRepository(TofiContext context) : base(context)
+        {
+        }
+    }
+}
