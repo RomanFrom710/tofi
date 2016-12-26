80c0747ef1c0974942293da3c0ec62ddd8fc5f76
Alex <alex.limontov@gmail.net>
2016-12-26 23:18:37 +0300

    UserAction model added

---
+++ TOFI/TOFI.DataAccess/Models/User/UserAction.cs
@@ -0,0 +1,22 @@
+﻿using DAL.Models.User;
+using Newtonsoft.Json;
+using System;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace DAL.Models.User
+{
+    [Table("UserActions")]
+    public class UserAction: Model
+    {
+        [JsonIgnore]
+        public string Signature { get; set; }
+
+        public DateTime Date { get; set; }
+
+        #region Virtual Properties
+
+        public virtual UserModel User { get; set; }
+
+        #endregion
+    }
+}
