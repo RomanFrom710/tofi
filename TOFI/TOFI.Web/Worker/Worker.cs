25899171ad62426ca9d9a4188bd91b2f089471f9
Alex <alex.limontov@gmail.com>
2016-12-30 00:48:42 +0300

    added primitive hangfire job

---
+++ TOFI/TOFI.Web/Worker/Worker.cs
@@ -0,0 +1,29 @@
+﻿using Hangfire;
+using System;
+using System.IO;
+
+namespace TOFI.Web.Worker
+{
+    public class Worker
+    {
+        internal static void Run()
+        {
+            BackgroundJob.Enqueue(() => Console.WriteLine("hello"));
+            var recurringJobManager = new RecurringJobManager();
+            RecurringJob.AddOrUpdate("credit-update", () => UpdateRemains(), Cron.Minutely);
+            //recurringJobManager.AddOrUpdate("credit-update", Job.FromExpression(() => UpdateRemains()), Cron.Minutely, new RecurringJobOptions());
+        }
+
+        private static void UpdateRemains()
+        {
+            using (var stream = new FileStream("file.txt", FileMode.OpenOrCreate))
+            {
+                using (var streamWriter = new StreamWriter(stream))
+                {
+                    streamWriter.WriteLine(DateTime.Now);
+                }
+            }
+            
+        }
+    }
+}
