23474e3241aea1a36f6d3da49069b6b37e1595be
Roman Chaley <romanchaley@gmail.com>
2017-01-01 14:52:52 +0300

    Happy New Year! Improved validation, minor UI changes

---
+++ TOFI/TOFI.BusinessLayer/Attributes/CustomRequiredAttribute.cs
@@ -0,0 +1,12 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace BLL.Attributes
+{
+    public class CustomRequiredAttribute: RequiredAttribute
+    {
+        public CustomRequiredAttribute()
+        {
+            ErrorMessage = "{0} — обязательное поле.";
+        }
+    }
+}
