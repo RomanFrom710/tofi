c6cce122ef4d49d3477a753b70dcff55cefaf97b
Alex <alex.limontov@gmail.net>
2017-01-02 21:35:01 +0300

    added CreditPayment model

---
+++ TOFI/TOFI.DataAccess/Repositories/Credits/CreditPayment/ICreditPaymentCommandRepository.cs
@@ -0,0 +1,9 @@
+﻿using DAL.Repositories.Model;
+using TOFI.TransferObjects.Credits.CreditPayment.DataObjects;
+
+namespace DAL.Repositories.Credits.CreditPayment
+{
+    public interface ICreditPaymentCommandRepository : IModelCommandRepository<CreditPaymentDto>
+    {
+    }
+}
