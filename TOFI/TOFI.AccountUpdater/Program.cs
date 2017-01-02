b33796e79f51ae5ca39e249631e2aa502638c6d6
Alex <alex.limontov@gmail.com>
2017-01-02 23:04:31 +0300

    webjob created

---
+++ TOFI/TOFI.AccountUpdater/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TOFI.AccountUpdater
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Trace.TraceInformation("from webjob " + DateTime.Now.ToString());
+        }
+    }
+}
