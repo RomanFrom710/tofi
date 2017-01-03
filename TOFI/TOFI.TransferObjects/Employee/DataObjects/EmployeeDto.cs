a61b7d30bcb2afe5a9cfae338fd14e53682e895c
Pavel Kindruk <kinpa200296@yandex.ru>
2017-01-04 02:10:36 +0300

    Added more roles.

--- TOFI/TOFI.TransferObjects/Employee/DataObjects/EmployeeDto.cs
+++ TOFI/TOFI.TransferObjects/Employee/DataObjects/EmployeeDto.cs
@@ -7,6 +7,11 @@ namespace TOFI.TransferObjects.Employee.DataObjects
     public enum EmployeeRights
     {
         None = 0,
+        Cashier = 1,
+        Operator = 2,
+        SecurityOfficer = 4,
+        CreditCommitteeMember = 8,
+        CreditDepartmentChief = 16,
         Admin = int.MaxValue,
     }
 
