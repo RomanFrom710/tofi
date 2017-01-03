5aba211dd6a0b886c5b180b95c6ba6412ccc1be2
Alex <alex.limontov@gmail.net>
2017-01-04 00:59:21 +0300

    updating all credit requests in 1 transaction

--- TOFI/TOFI.AccountUpdater/AccountUpdaterService.cs
+++ TOFI/TOFI.AccountUpdater/AccountUpdaterService.cs
@@ -1,37 +1,41 @@
-﻿using System.Linq;
-using DAL.Repositories.Credits.CreditAccount;
-using BLL.Services.Credits.CreditAccount;
+﻿using DAL.Repositories.Credits.CreditAccount;
 using TOFI.TransferObjects.Model.Queries;
 using DAL.Contexts;
 using NLog;
+using TOFI.TransferObjects.Model.Commands;
+using TOFI.TransferObjects.Credits.CreditAccount.DataObjects;
+using System.Linq;
 
 namespace TOFI.AccountUpdater
 {
     public class AccountUpdaterService
     {
-        private readonly CreditAccountService _creditAccountService;
         private readonly Logger Logger;
+        private readonly CreditAccountQueryRepository _creditAccountQueryRepository;
+        private readonly CreditAccountCommandRepository _creditAccountCommandRepository;
 
         public AccountUpdaterService()
         {
             Logger = LogManager.GetCurrentClassLogger(); 
             var context = new TofiContext();
-            var creditAccountService = new CreditAccountService(new CreditAccountQueryRepository(context), new CreditAccountCommandRepository(context));
-            _creditAccountService = creditAccountService;
+            _creditAccountCommandRepository = new CreditAccountCommandRepository(context);
+            _creditAccountQueryRepository = new CreditAccountQueryRepository(context);
         }
         
         public void UpdateRemains()
         {
             Logger.Info("UpdateRemains called");
             var query = new AllModelsQuery();
-            var value = _creditAccountService.GetAllModelDtos(query).Value;
-            var accounts = value.ToArray();
+            var accounts = _creditAccountQueryRepository.Handle(query);
             foreach (var account in accounts)
             {
-                account.RemainDebt.Value *= (decimal) (1 + account.InterestRate);
-                _creditAccountService.UpdateModel(account);
+                account.RemainDebt.Value *= (decimal)(1 + account.InterestRate);
             }
-            
+            var updateModelsCommand = new UpdateModelsCommand<CreditAccountDto>()
+            {
+                ModelsDto = accounts.ToList()
+            };
+            _creditAccountCommandRepository.Execute(updateModelsCommand);
         }
     }
 }
