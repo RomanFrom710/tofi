1da1ef52f9e6fd03f5e61afae4b168956f2ba49a
Pavel Kindruk <kinpa200296@yandex.ru>
2017-01-04 01:55:10 +0300

    Added support for credit payments.

---
+++ TOFI/TOFI.BusinessLayer/Services/Credits/CreditPayment/ViewModels/CreditPaymentViewModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using BLL.Services.Common.Price.ViewModels;
+using BLL.Services.Credits.CreditAccount.ViewModels;
+using BLL.Services.Model.ViewModels;
+
+namespace BLL.Services.Credits.CreditPayment.ViewModels
+{
+    public class CreditPaymentViewModel : ModelViewModel
+    {
+        public DateTime Timestamp { get; set; }
+
+        public PriceViewModel PaymentSum { get; set; }
+
+        public CreditAccountViewModel CreditAccount { get; set; }
+    }
+}
--- TOFI/TOFI.DataAccess/Repositories/Credits/CreditAccount/CreditAccountQueryRepository.cs
+++ TOFI/TOFI.DataAccess/Repositories/Credits/CreditAccount/CreditAccountQueryRepository.cs
@@ -1,7 +1,12 @@
-﻿using DAL.Contexts;
+﻿using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using DAL.Contexts;
 using DAL.Models.Credits.CreditAccount;
 using DAL.Repositories.Model;
 using TOFI.TransferObjects.Credits.CreditAccount.DataObjects;
+using TOFI.TransferObjects.Credits.CreditAccount.Queries;
+using TOFI.TransferObjects.Credits.CreditPayment.DataObjects;
 
 namespace DAL.Repositories.Credits.CreditAccount
 {
@@ -9,6 +14,23 @@ namespace DAL.Repositories.Credits.CreditAccount
     {
         public CreditAccountQueryRepository(TofiContext context) : base(context)
         {
+        }
+
+
+        public IEnumerable<CreditPaymentDto> Handle(CreditPaymentsQuery query)
+        {
+            var creditAccount = ModelsDao.Find(query.CreditAccountId);
+            if (creditAccount == null)
+                return Enumerable.Empty<CreditPaymentDto>();
+            return creditAccount.Payments.MapTo<CreditPaymentDto>();
+        }
+
+        public async Task<IEnumerable<CreditPaymentDto>> HandleAsync(CreditPaymentsQuery query)
+        {
+            var creditAccount = await ModelsDao.FindAsync(query.CreditAccountId);
+            if (creditAccount == null)
+                return Enumerable.Empty<CreditPaymentDto>();
+            return creditAccount.Payments.MapTo<CreditPaymentDto>();
         }
     }
 }
