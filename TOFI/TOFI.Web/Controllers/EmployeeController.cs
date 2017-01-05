bd530271126aa8ccb1f76feffac24f9796464bc3
Roman Chaley <romanchaley@gmail.com>
2017-01-06 00:03:18 +0300

    Credit approving is working!!!!!!!!!!

--- TOFI/TOFI.Web/Controllers/EmployeeController.cs
+++ TOFI/TOFI.Web/Controllers/EmployeeController.cs
@@ -1,10 +1,13 @@
 ﻿using System.Threading.Tasks;
 using System.Web.Mvc;
+using BLL.Services.Credits.CreditRequest;
 using BLL.Services.Employee;
+using BLL.Services.Employee.ViewModels;
 using BLL.Services.User;
 using Microsoft.AspNet.Identity;
 using TOFI.TransferObjects.Employee.Commands;
 using TOFI.TransferObjects.Employee.Queries;
+using TOFI.TransferObjects.Model.Queries;
 using TOFI.TransferObjects.User.Queries;
 using TOFI.Web.Infrastructure;
 
@@ -16,11 +19,15 @@ namespace TOFI.Web.Controllers
     {
         private readonly IUserService _userService;
         private readonly IEmployeeService _employeeService;
+        private readonly ICreditRequestService _creditRequestService;
 
-        public EmployeeController(IUserService userService, IEmployeeService employeeService)
+        public EmployeeController(IUserService userService,
+                                  IEmployeeService employeeService,
+                                  ICreditRequestService creditRequestService)
         {
             _userService = userService;
             _employeeService = employeeService;
+            _creditRequestService = creditRequestService;
         }
 
         public ActionResult Index()
@@ -28,19 +35,18 @@ namespace TOFI.Web.Controllers
             return View();
         }
 
-        public ActionResult RequestDetails(int id)
+        public ActionResult RequestDetails(int id, string type)
         {
-            return View();
+            ViewBag.ApproveActionName = type + "Approve";
+            var request = _creditRequestService.GetModel(new ModelQuery {Id = id}).Value;
+            return View(request);
         }
 
         [Authorize(Roles = "operator")]
         [HttpGet]
         public ActionResult Operator()
         {
-            var employee = _userService.GetUser(new UserQuery
-            {
-                Id = int.Parse(User.Identity.GetUserId())
-            }).Value.Employee;
+            var employee = GetEmployee();
             var requests =
                 _employeeService.GetOperatorCreditRequests(new OperatorCreditRequestsQuery {EmployeeId = employee.Id}).Value;
             return View(requests);
@@ -50,8 +56,19 @@ namespace TOFI.Web.Controllers
         [HttpPost]
         public ActionResult OperatorApprove(OperatorApproveCommand command)
         {
-            _employeeService.OperatorApproveCommand(new OperatorApproveCommand());
+            var employee = GetEmployee();
+            command.EmployeeId = employee.Id;
+            _employeeService.OperatorApproveCommand(command);
             return RedirectToAction("Operator");
+        }
+
+        private EmployeeViewModel GetEmployee()
+        {
+            var employee = _userService.GetUser(new UserQuery
+            {
+                Id = int.Parse(User.Identity.GetUserId())
+            }).Value.Employee;
+            return employee;
         }
     }
 }
