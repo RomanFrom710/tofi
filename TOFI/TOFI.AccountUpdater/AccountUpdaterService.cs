56acc1e3cd096e957120d3b8c83384461e7d76f5
Alex <alex.limontov@gmail.com>
2017-01-08 17:32:07 +0300

    changed AccountUpdaterService to update Accounts

--- TOFI/TOFI.AccountUpdater/AccountUpdaterService.cs
+++ TOFI/TOFI.AccountUpdater/AccountUpdaterService.cs
@@ -5,6 +5,10 @@ using NLog;
 using TOFI.TransferObjects.Model.Commands;
 using TOFI.TransferObjects.Credits.CreditAccount.DataObjects;
 using System.Linq;
+using System;
+using TOFI.TransferObjects.Credits.CreditAccount.Queries;
+using TOFI.TransferObjects.Common.Price.DataObjects;
+using System.Collections.Generic;
 
 namespace TOFI.AccountUpdater
 {
@@ -13,6 +17,8 @@ namespace TOFI.AccountUpdater
         private readonly Logger Logger;
         private readonly CreditAccountQueryRepository _creditAccountQueryRepository;
         private readonly CreditAccountCommandRepository _creditAccountCommandRepository;
+        private readonly CreditAccountStateQueryRepository _creditAccountStateQueryRepository;
+        private readonly CreditAccountStateCommandRepository _creditAccountStateCommandRepository;
 
         public AccountUpdaterService()
         {
@@ -20,22 +26,105 @@ namespace TOFI.AccountUpdater
             var context = new TofiContext();
             _creditAccountCommandRepository = new CreditAccountCommandRepository(context);
             _creditAccountQueryRepository = new CreditAccountQueryRepository(context);
+            _creditAccountStateCommandRepository = new CreditAccountStateCommandRepository(context);
+            _creditAccountStateQueryRepository = new CreditAccountStateQueryRepository(context);
         }
         
-        public void UpdateRemains()
+        public void UpdateAccounts()
         {
-            //Logger.Info("UpdateRemains called");
-            //var query = new AllModelsQuery();
-            //var accounts = _creditAccountQueryRepository.Handle(query);
-            //foreach (var account in accounts)
-            //{
-            //    account.RemainDebt.Value *= (decimal) (1 + account.CreditType.InterestRate);
-            //}
-            //var updateModelsCommand = new UpdateModelsCommand<CreditAccountDto>()
-            //{
-            //    ModelsDto = accounts.ToList()
-            //};
-            //_creditAccountCommandRepository.Execute(updateModelsCommand);
+            Logger.Info("UpdateRemains called");
+            var query = new AllModelsQuery();
+            var accounts = _creditAccountQueryRepository.Handle(query);
+            var creditAccountsStates = new List<CreditAccountStateDto>();
+            foreach (var account in accounts)
+            {
+                if (ShouldAccountUpdate(account))
+                {
+                    var accountCurrency = account.Currency;
+                    var creditAccountStatesQuery = new CreditAccountStatesQuery()
+                    {
+                        CreditAccountId = account.Id
+                    };
+                    var latestCreditAccountState = _creditAccountQueryRepository.Handle(creditAccountStatesQuery).AsEnumerable().OrderBy(s => s.Month).LastOrDefault();
+                    var creditAccountPaymentsQuery = new CreditPaymentsQuery()
+                    {
+                        CreditAccountId = account.Id
+                    };
+                    var latestCreditAccountStateDate = account.AgreementDate.AddMonths(latestCreditAccountState.Month);
+                    var paymentsForLatestPeriod = _creditAccountQueryRepository.Handle(creditAccountPaymentsQuery).Where(p => latestCreditAccountStateDate < p.Timestamp);
+                    // S
+                    var sumPaidForLatestPeriod = paymentsForLatestPeriod.Sum(p => p.PaymentSum.Value);
+                    // Z
+                    var totalDebtRemaining = latestCreditAccountState.RemainDebt.Value;
+                    // A
+                    var debtForMonth = totalDebtRemaining / (account.TotalMonthDuration - latestCreditAccountState.Month);
+                    // B
+                    var interestForMonth = (decimal)account.CreditType.InterestRate / 12 * totalDebtRemaining;
+                    var totalInterestNotPaid = latestCreditAccountState.TotalInterestSumNotPaid.Value;
+
+                    var finesForOverdue = 0m;
+                    var newTotalDebtRemaining = totalDebtRemaining;
+                    var newTotalInterestNotPaid = totalInterestNotPaid;
+                    if (sumPaidForLatestPeriod < debtForMonth)
+                    {
+                        finesForOverdue = account.CreditType.FineInterest * (debtForMonth - sumPaidForLatestPeriod);
+                        newTotalDebtRemaining -= sumPaidForLatestPeriod;
+                        newTotalInterestNotPaid += interestForMonth;
+                    }
+                    else if (sumPaidForLatestPeriod < debtForMonth + totalInterestNotPaid + interestForMonth)
+                    {
+                        newTotalDebtRemaining -= debtForMonth;
+                        newTotalInterestNotPaid += interestForMonth - (sumPaidForLatestPeriod - debtForMonth);
+                    }
+                    else
+                    {
+                        newTotalInterestNotPaid = 0m;
+                        newTotalDebtRemaining -= sumPaidForLatestPeriod - interestForMonth - totalInterestNotPaid;
+                    }
+
+                    var newCreditAccountState = new CreditAccountStateDto()
+                    {
+                        CreditAccount = account,
+                        Month = latestCreditAccountState.Month + 1,
+                        FinesForOverdue = new PriceDto()
+                        {
+                            Currency = accountCurrency,
+                            Value = finesForOverdue
+                        },
+                        InterestCounted = new PriceDto()
+                        {
+                            Currency = accountCurrency,
+                            Value = interestForMonth
+                        },
+                        RemainDebt = new PriceDto()
+                        {
+                            Currency = accountCurrency,
+                            Value = newTotalDebtRemaining + finesForOverdue
+                        },
+                        TotalInterestSumNotPaid = new PriceDto()
+                        {
+                            Currency = accountCurrency,
+                            Value = newTotalInterestNotPaid
+                        }
+                    };
+                    creditAccountsStates.Add(newCreditAccountState);
+                }
+            }
+
+            var updateModelsCommand = new UpdateModelsCommand<CreditAccountStateDto>()
+            {
+                ModelsDto = creditAccountsStates.ToList()
+            };
+            _creditAccountStateCommandRepository.Execute(updateModelsCommand);
         }
+
+        #region Private Methods
+
+        private static bool ShouldAccountUpdate(CreditAccountDto account)
+        {
+            return account.AgreementDate.Day == DateTime.Now.Day;
+        }
+
+        #endregion
     }
 }
