9b487709eb3a2d9201c8d887994d470b98fe1a3e
Pavel Kindruk <kinpa200296@yandex.ru>
2017-01-09 00:40:30 +0300

    Added employee method for making payments.

---
+++ TOFI/TOFI.TransferObjects/Employee/Commands/AddPaymentCommand.cs
@@ -0,0 +1,13 @@
+﻿using TOFI.TransferObjects.Common.Price.DataObjects;
+
+namespace TOFI.TransferObjects.Employee.Commands
+{
+    public class AddPaymentCommand : Command
+    {
+        public int CreditAccountId { get; set; }
+
+        public int EmployeeId { get; set; }
+
+        public PriceDto PaymentSum { get; set; }
+    }
+}
