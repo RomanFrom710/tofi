3c011cf0421892b126f23f2bdc44d2b67da4e633
Alex <alex.limontov@gmail.net>
2017-01-09 09:44:55 +0300

    search CreditAccount by AgreementNumber

--- TOFI/TOFI.TransferObjects/Credits/CreditAccount/Queries/ActualCreditAccountStateQuery.cs
+++ TOFI/TOFI.TransferObjects/Credits/CreditAccount/Queries/ActualCreditAccountStateQuery.cs
@@ -2,6 +2,18 @@
 {
     public class ActualCreditAccountStateQuery: Query
     {
-        public int CreditAccountId { get; set; }
+        public int? Id { get; set; }
+
+        public string AgreementNumber { get; set; }
+
+        public static ActualCreditAccountStateQuery WithId(int id)
+        {
+            return new ActualCreditAccountStateQuery { Id = id };
+        }
+
+        public static ActualCreditAccountStateQuery WithAgreementNumber(string agreementNumber)
+        {
+            return new ActualCreditAccountStateQuery { AgreementNumber = agreementNumber };
+        }
     }
 }
