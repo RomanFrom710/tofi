9d0db022ae502354d6b7e3cbc56686341d03550d
Pavel Kindruk <kinpa200296@yandex.ru>
2017-01-09 13:57:45 +0300

    Added Select CreditTypes Query.

---
+++ TOFI/TOFI.TransferObjects/Client/Queries/SelectCreditTypesQuery.cs
@@ -0,0 +1,13 @@
+﻿using TOFI.TransferObjects.Common.Price.DataObjects;
+
+namespace TOFI.TransferObjects.Client.Queries
+{
+    public class SelectCreditTypesQuery : Query
+    {
+        public int MonthDuration { get; set; }
+
+        public PriceDto CreditSum { get; set; }
+
+        public string CreditPurpose { get; set; }
+    }
+}
