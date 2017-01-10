fd7323b820f97e218cfd49efd0c7bbcb33e6fa93
Alex <alex.limontov@gmail.net>
2017-01-10 12:55:28 +0300

    calculating minimum payment required for credit selection

--- TOFI/TOFI.BusinessLayer/Services/Credits/BankCredits/CreditTypes/CreditTypeService.cs
+++ TOFI/TOFI.BusinessLayer/Services/Credits/BankCredits/CreditTypes/CreditTypeService.cs
@@ -1,6 +1,10 @@
-﻿using BLL.Services.Credits.BankCredits.CreditTypes.ViewModels;
+﻿using BLL.Services.Credits.BankCredits.CreditConditions.ViewModels;
+using BLL.Services.Credits.BankCredits.CreditTypes.ViewModels;
 using BLL.Services.Model;
 using DAL.Repositories.Credits.BankCredits.CreditTypes;
+using System.Linq;
+using TOFI.TransferObjects.Client.Queries;
+using TOFI.TransferObjects.Credits.BankCredits.CreditConditions.DataObjects;
 using TOFI.TransferObjects.Credits.BankCredits.CreditTypes.DataObjects;
 
 namespace BLL.Services.Credits.BankCredits.CreditTypes
@@ -16,6 +20,32 @@ namespace BLL.Services.Credits.BankCredits.CreditTypes
         {
             _queryRepository = queryRepository;
             _commandRepository = commandRepository;
+        }
+
+        public decimal MinMonthPayment(SelectCreditTypesQuery query, CreditTypeViewModel creditType)
+        {
+            return creditType.CreditConditions.Where(c => c.MinCreditSum.Currency.Id == query.CreditSumCurrencyId)
+            .Min(cond => GetMinMonthPaymentForCondition(creditType, cond));
+        }
+
+        public decimal MaxMonthPayment(SelectCreditTypesQuery query, CreditTypeViewModel creditType)
+        {
+            return creditType.CreditConditions.Where(c => c.MaxCreditSum.Currency.Id == query.CreditSumCurrencyId)
+            .Max(cond => GetMaxMonthPaymentForCondition(creditType, cond));
+        }
+
+        private decimal GetMinMonthPaymentForCondition(CreditTypeViewModel creditType, CreditConditionViewModel creditCondition)
+        {
+            var debt = creditCondition.MinCreditSum.Value / creditCondition.MonthDurationTo;
+            var interest = (decimal) creditType.InterestRate / 12 * creditCondition.MinCreditSum.Value;
+            return debt + interest;
+        }
+
+        private decimal GetMaxMonthPaymentForCondition(CreditTypeViewModel creditType, CreditConditionViewModel creditCondition)
+        {
+            var debt = creditCondition.MaxCreditSum.Value / creditCondition.MonthDurationFrom;
+            var interest = (decimal)creditType.InterestRate / 12 * creditCondition.MaxCreditSum.Value;
+            return debt + interest;
         }
     }
 }
