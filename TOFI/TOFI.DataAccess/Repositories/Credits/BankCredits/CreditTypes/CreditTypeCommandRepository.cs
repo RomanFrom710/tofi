048a40543a28e9ac8db23293e75fa9c001adaa81
Pavel Kindruk <kinpa200296@yandex.ru>
2017-01-10 14:30:26 +0300

    Fixed credit type repository.

--- TOFI/TOFI.DataAccess/Repositories/Credits/BankCredits/CreditTypes/CreditTypeCommandRepository.cs
+++ TOFI/TOFI.DataAccess/Repositories/Credits/BankCredits/CreditTypes/CreditTypeCommandRepository.cs
@@ -1,4 +1,8 @@
-﻿using DAL.Contexts;
+﻿using System.Collections.Generic;
+using AutoMapper;
+using DAL.Contexts;
+using DAL.Models.Credits.BankCredits.CreditConditions;
+using DAL.Models.Credits.BankCredits.CreditRequirements;
 using DAL.Models.Credits.BankCredits.CreditTypes;
 using DAL.Repositories.Model;
 using TOFI.TransferObjects.Credits.BankCredits.CreditTypes.DataObjects;
@@ -9,6 +13,47 @@ namespace DAL.Repositories.Credits.BankCredits.CreditTypes
     {
         public CreditTypeCommandRepository(TofiContext context) : base(context)
         {
+        }
+
+
+        protected override CreditTypeModel CreateDbModel(CreditTypeDto modelDto)
+        {
+            var model = base.CreateDbModel(modelDto);
+            CreateConditionsAndRequirements(model, modelDto);
+            return model;
+        }
+
+        protected override void UpdateDbModel(CreditTypeModel model, CreditTypeDto modelDto)
+        {
+            base.UpdateDbModel(model, modelDto);
+            foreach (var condition in model.CreditConditions)
+            {
+                Context.CreditConditions.Remove(condition);
+            }
+            foreach (var requirement in model.CreditRequirements)
+            {
+                Context.CreditRequirements.Remove(requirement);
+            }
+            CreateConditionsAndRequirements(model, modelDto);
+        }
+
+
+        private void CreateConditionsAndRequirements(CreditTypeModel model, CreditTypeDto modelDto)
+        {
+            model.CreditConditions = new List<CreditConditionModel>();
+            model.CreditRequirements = new List<CreditRequirementModel>();
+            foreach (var condition in modelDto.CreditConditions)
+            {
+                var condModel = Mapper.Map<CreditConditionModel>(condition);
+                condModel.MinCreditSum.Currency = GetCurrencyModel(condition.MinCreditSum.Currency.Id);
+                condModel.MaxCreditSum.Currency = GetCurrencyModel(condition.MaxCreditSum.Currency.Id);
+                model.CreditConditions.Add(condModel);
+            }
+            foreach (var requirement in modelDto.CreditRequirements)
+            {
+                var reqModel = Mapper.Map<CreditRequirementModel>(requirement);
+                model.CreditRequirements.Add(reqModel);
+            }
         }
     }
 }
